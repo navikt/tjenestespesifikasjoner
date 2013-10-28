138f5701c9f29351028e9c348d779c75ab89aead
Oyvind Nordsveen <oyvind.nordsveen@nav.no>
2013-10-28 08:46:49 +0100

    Mapping av xs:date til DateTime

--- utbetaling/src/main/java/no/nav/sbl/dialogarena/utbetaling/adapters/DateTimeAdapter.java
+++ utbetaling/src/main/java/no/nav/sbl/dialogarena/utbetaling/adapters/DateTimeAdapter.java
@@ -1,15 +1,25 @@
 package no.nav.sbl.dialogarena.utbetaling.adapters;
 
 import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
 
 public class DateTimeAdapter {
 
-    public static DateTime unmarshal(String v) {
-        return new DateTime(v);
-    }
+        public static DateTime unmarshal(String v) {
+            return new DateTime(v);
+        }
 
-    public static String marshal(DateTime v) {
-        return v.toString();
-    }
+        public static String marshal(DateTime v) {
+            return v.toString();
+        }
+//
+//    public static LocalDate unmarshal(String v) {
+//        return new LocalDate(v);
+//    }
+//
+//    public static String marshal(LocalDate v) {
+//        return v.toString();
+//    }
+
 
 }
