e1ec34e61272a5def9bbbac8f63618e58e3d3b00
Borge Bardo <borge.bardo@nav.no>
2014-02-14 10:55:35 +0100

    lagt til enkel datetimeadapter med statiske metoder hvis man ikke trenger all fancyness som XmlAdapter gir

---
+++ jaxb-adapters/src/main/java/no/nav/sbl/dialogarena/adapters/simple/DateTimeAdapter.java
@@ -0,0 +1,14 @@
+package no.nav.sbl.dialogarena.adapters.simple;
+
+import org.joda.time.DateTime;
+
+public class DateTimeAdapter {
+
+    public static DateTime unmarshal(String v) {
+        return new DateTime(v);
+    }
+
+    public static String marshal(DateTime v) {
+        return v.toString();
+    }
+}
