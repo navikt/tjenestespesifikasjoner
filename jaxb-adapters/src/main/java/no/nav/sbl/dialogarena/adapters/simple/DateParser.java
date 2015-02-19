5f9aab297c2e84f79630eee31fda7305415fd530
Andreas Baakind <andreas.baakind@nav.no>
2015-02-19 09:51:43 +0100

    JAXB-converter med ny static DateParser for Gregorian Date objekter

---
+++ jaxb-adapters/src/main/java/no/nav/sbl/dialogarena/adapters/simple/DateParser.java
@@ -0,0 +1,31 @@
+package no.nav.sbl.dialogarena.adapters.simple;
+
+import org.joda.time.DateTime;
+
+import java.text.SimpleDateFormat;
+
+public class DateParser {
+
+    private static final String DATE_FORMAT = "yyyy-MM-dd+HH:mm";
+
+    public static DateTime unmarshal(String v) {
+        if(v.contains("Z")) {
+            return new DateTime(v.substring(0, v.indexOf('Z')));
+        } else if(v.contains("+")) {
+            int idx = v.indexOf("+");
+            int hour = Integer.valueOf(v.substring(idx + 1, idx + 3));
+            int minute = Integer.valueOf(v.substring(idx + 4, idx + 6));
+
+            return new DateTime(v.substring(0, v.indexOf('+')))
+                    .withHourOfDay(hour)
+                    .withMinuteOfHour(minute);
+        }
+
+        return new DateTime(v);
+    }
+
+    public static String marshal(DateTime v) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(DATE_FORMAT);
+        return simpleDateFormat.format(v.toDate());
+    }
+}
---
+++ jaxb-adapters/src/test/java/no/nav/sbl/dialogarena/adapters/simple/DateParserTest.java
@@ -0,0 +1,54 @@
+package no.nav.sbl.dialogarena.adapters.simple;
+
+import org.joda.time.DateTime;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+
+public class DateParserTest {
+
+    @Test
+    public void unmarshalYearhMonthDay() {
+        String dateString = "2014-10-09";
+        DateTime dateTime = DateParser.unmarshal(dateString);
+        assertThat(dateTime.getYear(), is(2014));
+        assertThat(dateTime.getMonthOfYear(), is(10));
+        assertThat(dateTime.getDayOfMonth(), is(9));
+    }
+
+    @Test
+    public void unmarshalYearhMonthDayWithHourMinute() {
+        String dateString = "2014-10-09+02:03";
+        DateTime dateTime = DateParser.unmarshal(dateString);
+        assertThat(dateTime.getYear(), is(2014));
+        assertThat(dateTime.getMonthOfYear(), is(10));
+        assertThat(dateTime.getDayOfMonth(), is(9));
+        assertThat(dateTime.getHourOfDay(), is(2));
+        assertThat(dateTime.getMinuteOfHour(), is(3));
+    }
+
+    @Test
+    public void unmarshalYearhMonthDayWithZulu() {
+        String dateString = "2014-10-09Z";
+        DateTime dateTime = DateParser.unmarshal(dateString);
+        assertThat(dateTime.getYear(), is(2014));
+        assertThat(dateTime.getMonthOfYear(), is(10));
+        assertThat(dateTime.getDayOfMonth(), is(9));
+    }
+
+    @Test
+    public void marshalYearhMonthDay() {
+        DateTime dateTime = new DateTime(2014, 10, 9, 0, 0);
+        String dateString = DateParser.marshal(dateTime);
+        assertThat(dateString, is("2014-10-09+00:00"));
+    }
+
+    @Test
+    public void marshalYearhMonthDayWithHourMinute() {
+        DateTime dateTime = new DateTime(2014, 10, 9, 1, 2);
+        String dateString = DateParser.marshal(dateTime);
+        assertThat(dateString, is("2014-10-09+01:02"));
+    }
+}
