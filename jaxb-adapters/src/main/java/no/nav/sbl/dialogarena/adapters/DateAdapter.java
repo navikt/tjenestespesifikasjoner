10606ddeccc290fda709efce243f352cfc468b66
Andreas Baakind <andreas.baakind@nav.no>
2015-02-19 08:52:15 +0100

    Revert "JAXB DateAdapter er static"
    This reverts commit af02f51f70dbd209947726bc1b62e89002fc3b77.

--- jaxb-adapters/src/main/java/no/nav/sbl/dialogarena/adapters/DateAdapter.java
+++ jaxb-adapters/src/main/java/no/nav/sbl/dialogarena/adapters/DateAdapter.java
@@ -1,20 +1,20 @@
 package no.nav.sbl.dialogarena.adapters;
 
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
 import org.joda.time.LocalDate;
 import org.joda.time.format.ISODateTimeFormat;
 
-import javax.xml.bind.annotation.adapters.XmlAdapter;
-
 public class DateAdapter extends XmlAdapter<String, LocalDate> {
 
-	public static LocalDate unmarshal(String v) {
+	public LocalDate unmarshal(String v) {
 		if (v.contains("+")) {
 			return new LocalDate(v.substring(0, v.indexOf('+')));
 		}
 		return new LocalDate(v);
 	}
 
-	public static String marshal(LocalDate v) {
+	public String marshal(LocalDate v) {
 		return v != null ? v.toString(ISODateTimeFormat.date()) : null;
 	}
 
