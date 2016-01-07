c9c518ec21c7d90dcd974069c47139efd13eec9e
Tine Kleivane <tine.kleivane@nav.no>
2016-01-07 13:23:10 +0100

    PK-258547 Legg på felles pålogging

--- fillager/src/main/upload-attachment.js
+++ fillager/src/main/upload-attachment.js
@@ -3,6 +3,13 @@
 var request = require('request');
 var fs = require('fs');
 
+request = request.defaults({
+    auth: {
+        username: 'srvPortaler',
+        password: 'PyTjo3mED6Q96c'
+    }
+});
+
 fs.readdir('./target/generated-resources/xml/xslt', function (err, files) {
     var filnavn = files[0];
     console.log('Filen skal lastes opp er: ', filnavn);
@@ -10,11 +17,7 @@ fs.readdir('./target/generated-resources/xml/xslt', function (err, files) {
     request.get(
         {
             url: 'http://confluence.adeo.no/rest/api/content/162739803/child/attachment',
-            json: 'json',
-            auth: {
-                username: 'srvPortaler',
-                password: 'PyTjo3mED6Q96c'
-            }
+            json: 'json'
         }, function (error, response, body) {
             if (error) {
                 return console.error(error)
@@ -25,7 +28,8 @@ fs.readdir('./target/generated-resources/xml/xslt', function (err, files) {
                 return attachment.title === filnavn;
             });
             if (attachment.length > 0) {
-                console.log('PUT');
+                console.log('Oppdater eksisterende vedlegg');
+
                 var attachmentId = attachment[0].id;
 
                 var formData = {
@@ -35,10 +39,6 @@ fs.readdir('./target/generated-resources/xml/xslt', function (err, files) {
 
                 request.post({
                         url: 'http://confluence.adeo.no/rest/api/content/162739803/child/attachment/att162743363/data',
-                        auth: {
-                            username: 'srvPortaler',
-                            password: 'PyTjo3mED6Q96c'
-                        },
                         headers: {
                             'X-Atlassian-Token': 'nocheck'
                         },
