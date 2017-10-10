fd0e9161ed1d10e30a0876b845e059d625d41f29
Tia Firing <tia.firing@nav.no>
2017-10-10 12:39:36 +0200

    midlertidige endringer i sak-v2

--- sak-v2/src/main/java/no/nav/tjeneste/virksomhet/sak/v2/feil/WSUgyldigInputException.java
+++
@@ -1,9 +0,0 @@
-package no.nav.tjeneste.virksomhet.sak.v2.feil;
-
-public class WSUgyldigInputException extends Exception {
-    public WSUgyldigInputException() {}
-
-    public WSUgyldigInputException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
