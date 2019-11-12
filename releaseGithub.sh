acb8e4a60ffbf983d10187e95c782c7fe151127c
Eirik Sletteberg <eirik.sletteberg@nav.no>
2019-11-12 13:18:40 +0100

    Try releasing to Github Package Registry

---
+++ releaseGithub.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+echo "Importing GPG key into keyring"
+echo $GPG_KEY_BASE64 | base64 -d | gpg --yes --batch --fast-import
+
+TIME=$(TZ="Europe/Oslo" date +%Y.%m.%d-%H.%M)
+COMMIT=$(git rev-parse --short=12 HEAD)
+VERSION="1.$TIME-$COMMIT"
+echo "Setting version $VERSION"
+
+mvn -B versions:set -DnewVersion="$VERSION"
+mvn -B versions:commit
+
+echo "Running release"
+export GPG_TTY=$(tty) && mvn -B --settings maven-settings.xml deploy -PreleaseGithub -Dgithub.token=$GITHUB_TOKEN # -DskipTests=true
