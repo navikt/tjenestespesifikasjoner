4790849476463fa20311414c99f33a2cac0fa8e5
Eirik Sletteberg <eirik.sletteberg@nav.no>
2020-10-15 13:22:54 +0200

    New release attempt

--- releaseGithub.sh
+++ releaseGithub.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-TIME=$(TZ="Europe/Oslo" date +%Y.%m.%d.%H.%M)
+TIME=$(TZ="Europe/Oslo" date +%Y.%m.%d-%H.%M)
 COMMIT=$(git rev-parse --short=12 HEAD)
 VERSION="1.$TIME-$COMMIT"
 echo "Setting version $VERSION"
 
-mvn -B versions:set -DnewVersion="$VERSION"
-mvn -B versions:commit
+mvn -B versions:set -DnewVersion="$VERSION" >/dev/null
+mvn -B versions:commit >/dev/null
 
 echo "Running release"
-mvn -B --settings maven-settings.xml deploy -Dmaven.wagon.http.pool=false -DretryFailedDeploymentCount=5
+mvn -B -X --settings maven-settings.xml deploy -Dmaven.wagon.http.pool=false -DretryFailedDeploymentCount=5 -pl :arbeidOgAktivitetVedtak-v1-tjenestespesifikasjon
