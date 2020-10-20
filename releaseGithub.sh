97e7176c2ea017547a629dc6a9cdd733bccba7d9
Eirik Sletteberg <eirik.sletteberg@nav.no>
2020-10-20 10:25:31 +0200

    Revert "New release attempt"
    This reverts commit 4790849476463fa20311414c99f33a2cac0fa8e5.

--- releaseGithub.sh
+++ releaseGithub.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-TIME=$(TZ="Europe/Oslo" date +%Y.%m.%d-%H.%M)
+TIME=$(TZ="Europe/Oslo" date +%Y.%m.%d.%H.%M)
 COMMIT=$(git rev-parse --short=12 HEAD)
 VERSION="1.$TIME-$COMMIT"
 echo "Setting version $VERSION"
 
-mvn -B versions:set -DnewVersion="$VERSION" >/dev/null
-mvn -B versions:commit >/dev/null
+mvn -B versions:set -DnewVersion="$VERSION"
+mvn -B versions:commit
 
 echo "Running release"
-mvn -B -X --settings maven-settings.xml deploy -Dmaven.wagon.http.pool=false -DretryFailedDeploymentCount=5 -pl :arbeidOgAktivitetVedtak-v1-tjenestespesifikasjon
+mvn -B --settings maven-settings.xml deploy -Dmaven.wagon.http.pool=false -DretryFailedDeploymentCount=5
